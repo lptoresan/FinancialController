ccb99d27912e075a888eaca361bc03c9a870393b
lptoresan <lptoresanb0906@hotmail.com>
2023-06-25 22:58:35 -0300

    Desenvolvimento aplicação de controle financeiro

---
+++ frontend/src/utils/menuItems.js
@@ -0,0 +1,22 @@
+import {dashboard, expenses, transactions, trend} from '../utils/Icons'
+
+export const menuItems = [
+    {
+        id: 1,
+        title: 'Dashboard',
+        icon: dashboard,
+        link: '/dashboard'
+    },
+    {
+        id: 3,
+        title: "Entradas",
+        icon: trend,
+        link: "/dashboard",
+    },
+    {
+        id: 4,
+        title: "Gastos",
+        icon: expenses,
+        link: "/dashboard",
+    },
+]
